caa2e6fa90fc3146a8d5a5cf60b7f7b4a920dbe2
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-03-17 14:59:41 -0400

    refactoring unesco munging

--- analysis.R
+++ analysis.R
@@ -22,8 +22,44 @@ lit.prod <- subset(book.prod,select=c(Country,Literature.4))
 lit.prod$prod.year <- 1999 # by default
 names(lit.prod) <- c("Country","Literature","prod.year")
 
+# initialize a data column from 1999 data and then
+# attempt to supply missing values iteratively
+# going backwards in time
+# df: original imported unesco data table
+# col: type of column we're operating on
+# return value: a copy of df with col and col.year columns
+# col has the values filled in,
+# col.year indicates what year the value is from
+unesco.fill.missing <- function(df,col="Literature") {
+  # name of column to hold what year we've gotten data from
+  year.col <- paste(col,".year",sep="")
+  filled.col <- paste(col,".filled",sep="")
+  result <- df
+  # initialize to all NA's
+  result[[filled.col]] <- NA
+  for(i in 4:0) {
+    # what's still missing?
+    missing <- is.na(result[[filled.col]])
+    result[[year.col]][missing] <- 1995 + i
+    # because the raw UNESCO data has five identical "Literature"
+    # columns (similarly for all other categories),
+    # the data column is called e.g. "Literature.3" for 1998
+    # but just "Literature" for 1995
+    data.col <- ifelse(i > 0,paste(col,".",i,sep=""),col)
+    
+    result[[filled.col]][missing] <- df[[data.col]][missing]
+  }
+  # but now we've accidentally filled in 1995 for the totally
+  # missing rows, too
+  missing <- is.na(result[[filled.col]])
+  result[[year.col]][missing] <- NA
+  
+  # try to get the data types right, R
+  result[[filled.col]] <- as.numeric(as.character(result[[filled.col]]))
+  result
+}
+
 # attempt to supply missing values
-# this will generate some warnings about NA's
 # first from 1998
 missing.lits <- is.na(lit.prod$Literature)
 lit.prod$prod.year[missing.lits] <- 1998
@@ -91,6 +127,15 @@ for(i in seq_along(countries.df$Country)) {
     countries.df$Freq[i] / country.lit.prod[as.character(countries.df$Country[i])]
 }
 
+top.in.year <- function(df,year,n=10,per.book=TRUE) {
+  rows <- subset(df,subset=(Year==year))
+  if(per.book) {
+    return(rows[order(rows$per.book,decreasing=TRUE)[1:n],])
+  }
+  else {
+    return(rows[order(rows$per.capita,decreasing=TRUE)[1:n],])
+  }
+}
 
 # per language
 # TODO language per capita counts
